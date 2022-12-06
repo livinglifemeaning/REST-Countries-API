b2281d92a88ef8cbb8d55cd09c31620d05bf52f9
Asha Cole <maymunahrose@gmail.com>
2022-12-06 13:43:28 -0500

    added earth loading page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+
 const index = () => {
   const router = useRouter();
   useEffect(() => {
     router.push("/countries");
   }, []);
 
-  return <div>Loading...</div>;
+  return (
+    <div style={{ width: "100%", display: "grid", placeContent: "center" }}>
+      <img src="/earth.png" alt="" />
+    </div>
+  );
 };
 
 export default index;
